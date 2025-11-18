96df3f86674680fbef96387395d2254989ae3ced
Sanjai <ksanjai427@gmail.com>
2025-11-19 03:39:15 +0530

    Fix: Remove preloader to show app immediately

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Preloader from './Preloaders/preloader1.jsx';
 import './index.css';
 
-function Root() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 3000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <React.StrictMode>
-      {/* <App /> */}
-      {loading ? <Preloader /> : <App />}
-    </React.StrictMode>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
